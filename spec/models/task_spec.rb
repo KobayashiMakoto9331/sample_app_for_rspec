0b3ea5fa57ccafd94b8300e60f7cd85559c6869c
小林誠 <kobax0@gmail.com>
2021-05-02 16:33:34 +0900

    モデルのバリデーションに関するテストを追加

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'validation' do
+
+    #title, status がなければ無効であること
+    it "is invalid with a title, status" do
+      task = build(:task)
+      expect(task).to be_valid
+      expect(task.errors).to be_empty
+    end
+
+    #titleがなければ無効であること
+    it "is invalid with a title" do
+      task = build(:task, title: nil)
+      expect(task).to be_invalid
+      expect(task.errors[:title]).to include("can't be blank")
+    end
+
+    #statusがなければ無効であること
+    it "is invalid with a status" do
+      task = build(:task, status: nil)
+      expect(task).to be_invalid
+      expect(task.errors[:status]).to include("can't be blank")
+    end
+    
+    #重複したtitleは無効であること
+    it "is invalid with a duplicate title" do
+      FactoryBot.create(:task, title: "same_tatle")
+      new_task = FactoryBot.build(:task, title: "same_tatle")
+      new_task.valid?
+      expect(new_task.errors[:title]).to include("has already been taken")
+    end
+
+    #異なるtitleである時有効であること
+    it "is valid with another title" do
+      FactoryBot.create(:task)
+      new_task = FactoryBot.build(:task)
+      expect(new_task).to be_valid
+    end
+
+  end
 end
